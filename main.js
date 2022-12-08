9289f2146ee60e4174250569255a53d6bcfbe187
abduvohidovnurmuhammad <abduvohidovn7@gmail.com>
2022-12-08 20:32:29 +0500

    svitafor

---
+++ main.js
@@ -0,0 +1,15 @@
+let svitafor = document.querySelector("body")
+let svitafor2 = document.querySelector("body")
+let svitafor3 = document.querySelector("body")
+let red = "red"
+let yellow = "yellow"
+let green = "green"
+setInterval(() => { 
+    svitafor.style.backgroundColor = 'red'
+}, 1000)
+setInterval(() => { 
+    svitafor2.style.backgroundColor = 'yellow'
+}, 2000)
+setInterval(() => { 
+    svitafor3.style.backgroundColor = 'green'
+}, 2500)
